9e5f989d845d002c9b5608842ab6b77c490b7f8a
trevor <travahnetshisaulu@gmail.com>
2026-09-06 21:39:53 +0200

    setting sass

--- src/nav/TopNavbar.tsx
+++ src/nav/TopNavbar.tsx
@@ -6,7 +6,7 @@ import type { User } from '../account/types/user';
 import { useAuth } from '../account/context/AuthContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //@ts-ignore
-import styles from  './TopNavbar.module.css';
+import styles from  './TopNavbar.module.scss';
 
 interface TopNavbarProps {
   user?: User | null;
